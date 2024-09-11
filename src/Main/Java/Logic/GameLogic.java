bd9ce978f191f03582da8d729073023d9d4328da
Kyle Purser <kylepurser0@gmail.com>
2024-09-10 23:29:31 -0500

    updates

--- src/Main/Java/Logic/GameLogic.java
+++ src/Main/Java/Logic/GameLogic.java
@@ -137,5 +137,4 @@ public class GameLogic {
         player.setCurrentRoom(rooms.get("StartRoom"));
     }
     
-    
 }
--- src/Main/Java/Logic/Main.java
+++ src/Main/Java/Logic/Main.java
@@ -1,22 +1,12 @@
+package Main.Java.Logic;
+
+import java.io.FileNotFoundException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Bandit a = new Bandit(1);
-		Bat b = new Bat(1);
-		AncientGoldDragon c = new AncientGoldDragon(1);
-		
-		System.out.println(a.getName());
-		System.out.println(a.getArmorClass());
-		System.out.println(a.getSpeed());
-		System.out.println(a.getChallengeRating());
-		System.out.println(b.getName());
-		System.out.println(b.getArmorClass());
-		System.out.println(b.getSpeed());
-		System.out.println(b.getChallengeRating());
-		System.out.println(c.getName());
-		System.out.println(c.getArmorClass());
-		System.out.println(c.getSpeed());
-		System.out.println(c.getChallengeRating());
+	public static void main(String[] args) throws FileNotFoundException {
+		GameLogic game = new GameLogic();
+		game.startGame();
 	}
 
 }
