5d5e66d6f61cd8b85b44ed8d96de92f5d91ed94d
patowek <101425532+patowek@users.noreply.github.com>
2024-09-16 20:43:44 +0100

    a

--- src/Main/Java/Classes/AdventurerTest.java
+++ src/Main/Java/Classes/AdventurerTest.java
@@ -1,7 +1,6 @@
 package Classes;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,22 +10,13 @@ class AdventurerTest {
 	void valueShouldEqual0() {
 		var adv = new Adventurer();
 		String[] str = {"helmet", "boot"};
-		String[] str1 = {"nothing","something"};
 		assertTrue(adv.findValue(str, "helmet")==0);
+	}
+	@Test
+	void valueShouldEqualNeg1() {
+		var adv = new Adventurer();
+		String[] str1 = {"nothing","something"};
 		assertTrue(adv.findValue(str1, "boot")==-1);
 	}
 
 }
-
-/*
- * 	public int findValue(String[] values, String query) {// Return where the value is found if the query is found.
-		for (int i = 0; i < values.length; i++) {
-			if (values[i] == query) {
-				System.out.println("Array contains" + query + "  at index " + i);
-				return i;
-			}
-		}
-		System.out.println("Value not Found.");
-		return -1;
-	}
- */
