a30e22567885e87938c5eccb9a92507144120858
Kyle Purser <kylepurser0@gmail.com>
2024-09-24 19:13:23 -0500

    Altered the Attributes to reflect new stats

--- src/Main/Java/Classes/Attributes.java
+++ src/Main/Java/Classes/Attributes.java
@@ -1,12 +1,6 @@
 package Classes;
 
 public enum Attributes {
-	STR("Strength", 10),
-	DEX("Dexterity", 10),
-	CON("Constitution", 10),
-	INT("Intelligence", 10),
-	WIS("Wisdom", 10),
-	CHA("Charisma", 10),
 	ATK("Attack Power", 0),
 	MAG("Magic Power", 0),
 	DEF("Defense", 0),
