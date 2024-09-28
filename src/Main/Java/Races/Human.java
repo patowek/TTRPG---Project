62940a3cbcdbbab6d5e350952791ceb78da737fa
IvanielMorales <89319230+IvanielMorales@users.noreply.github.com>
2024-09-27 22:56:46 -0500

    Update Human.java

--- src/Main/Java/Races/Human.java
+++ src/Main/Java/Races/Human.java
@@ -37,30 +37,14 @@ public class Human extends Adventurer {
 	public Human() {// Human("Lizette",1,3,3,2,2,2,2,10,10)
 					// Basic already built non-customized object
 		name = "Lizette";
-		strength = 1;
-		intelligence = 3;
-		wisdom = 3;
-		constitution = 2;
-		dexterity = 2;
-		charisma = 2;
-		gold = 10;
-		health = 10;
-		mana = 10;
 	}
 
 	public Human(String inName, int inStrength, int inIntelligence, int inWisdom, int inConstitution, int inDexterity,
-			int inCharisma, int inGold, int inHealth, int inMana) {// Constructor for the elf object. Where you can
+			int inCharisma, Attributes inGold, Attributes inHealth, int inMana) {// Constructor for the elf object. Where you can
 																	// allocate HP
 		name = inName;
-		strength = inStrength;
-		intelligence = inIntelligence;
-		wisdom = inWisdom;
-		constitution = inConstitution;
-		dexterity = inDexterity;
-		charisma = inCharisma;
 		gold = inGold;
 		health = inHealth;
-		mana = inMana;
 
 	}
 
