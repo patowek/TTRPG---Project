80ec6555c96c264159fd84dedb1cba75513a10ba
Kyle Purser <kylepurser0@gmail.com>
2024-10-07 15:48:55 -0500

    Merge branch 'main' of https://github.com/patowek/TTRPG---Project.git into main

--- src/Main/Java/Enemies/Enemies.java
+++ src/Main/Java/Enemies/Enemies.java
@@ -7,24 +7,14 @@ public class Enemies {
 	private int armor;
 	private int speed;
 	private int challengeRating;
-	private int multiplier;
 
 	public Enemies(String[] line) {
 		uid = line[0].trim();
 		name = line[1].trim();
-		hitpoints = Integer.parseInt(line[2].trim());
 		armor = Integer.parseInt(line[3].trim());
 		speed = Integer.parseInt(line[4].trim());
 		challengeRating = Integer.parseInt(line[5].trim());
-	}
-	
-	public Enemies(String uid, String name, int hitpoints, int armor, int speed, int challengeRating){
-		this.uid = uid;
-		this.name = name;
-		this.hitpoints = hitpoints;
-		this.armor = armor;
-		this.speed = speed;
-		this.challengeRating = challengeRating;
+		hitpoints = Integer.parseInt(line[2].trim()) * challengeRating;
 	}
 
 	public String getUid() {
@@ -36,7 +26,7 @@ public class Enemies {
 	}
 
 	public int getHitpoints() {
-		return hitpoints * multiplier;
+		return hitpoints;
 	}
 
 	public int getArmor() {
@@ -49,10 +39,6 @@ public class Enemies {
 
 	public int getChallengeRating() {
 		return challengeRating;
-	}
-
-	public int getMultiplier() {
-		return multiplier;
 	}
 
 	public void setHitpoints(int hitpoints) {
